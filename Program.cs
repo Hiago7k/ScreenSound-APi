bd4843a05c19a4b43561ae30c59b7e70fcd66198
Hiago 7k <hiago7k@gmail.com>
2026-03-16 20:16:24 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Chamando bibilioteca http, para conseguir trabalhar com API
+// Obs nao colocamos ; no final pois vamos usar somente dentro do using
+// using aqui entra, como gerenciando um recurso que vai ser usado ate a chave fechar }
+
+using (HttpClient client = new HttpClient()) 
+{
+    // Nossa api tudo e string
+    // Entao nossa primeira resposta e do tipo STRING
+    // entao vamos usar a seguinte variavel
+    // utilizamos o get, para pegar string e o Async e por que nao sabemos
+    // a quantidade de musicas ou qual tamanho desse recurso que estamos recebendo
+    // e nos queremos garantir que vamos conseguir receber. todos esses recursos
+    string resposta = client.GetStringAsync
+}
