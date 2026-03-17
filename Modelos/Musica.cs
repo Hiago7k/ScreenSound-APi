f1b0a72461afd0a76504e31ebd57635290f7a0fa
Hiago 7k <hiago7k@gmail.com>
2026-03-17 18:51:56 -0300

    usando json para referencia uma propriedade

--- Modelos/Musica.cs
+++ Modelos/Musica.cs
@@ -1,7 +1,9 @@
-﻿
+﻿using System.Text.Json.Serialization;
 
 namespace ScreenSound_APi.Modelos;
-
 internal class Musica
 {
+   
+    [JsonPropertyName("song")]
+    public string Nome { get; set; }
 }
