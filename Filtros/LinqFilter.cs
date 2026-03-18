b45c70f7c9d4e36215e37aed2b485b51d22adbea
Hiago 7k <hiago7k@gmail.com>
2026-03-18 18:16:59 -0300

    criando nosso metodo filtro utilizando LINQ

---
+++ Filtros/LinqFilter.cs
@@ -0,0 +1,10 @@
+﻿using ScreenSound_APi.Modelos;
+using System.Linq;
+namespace ScreenSound_APi.Filtros;
+
+internal class LinqFilter
+{
+    public static void FiltrarTodosOsGenerosMusicais(List<Musica> musicas) 
+    {
+    }
+}
