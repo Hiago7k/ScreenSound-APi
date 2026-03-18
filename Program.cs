f4a39b90f0399d5ea88a3cfd503db8f5ebf709b2
Hiago 7k <hiago7k@gmail.com>
2026-03-18 18:25:21 -0300

    trazendo os dados da lista

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using ScreenSound_APi.Modelos;
 using System.Text.Json;
+using ScreenSound_APi.Filtros;
 
 using (HttpClient client = new HttpClient()) 
 { 
@@ -7,7 +8,7 @@ using (HttpClient client = new HttpClient())
     {
         string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
         var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
-        musicas[1998].ExibirDetalhesDaMusica();
+        LinqFilter.FiltrarTodosOsGenerosMusicais(musicas);
     }
     catch (Exception ex)
     {
