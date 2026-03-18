4a4ddab096df8afa4b21e0cbfdbb74b98c2b7be0
Hiago 7k <hiago7k@gmail.com>
2026-03-18 18:40:33 -0300

    filtro pelo nome

--- Filtros/LinqOrder.cs
+++ Filtros/LinqOrder.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using ScreenSound_APi.Modelos;
 namespace ScreenSound_APi.Filtros;
 
 internal class LinqOrder
 {
-    public static void ExibirListaDeArtistasOrdernados() { }
+    public static void ExibirListaDeArtistasOrdernados(List<Musica> musicas) 
+    {
+        var artistasOrdenados = musicas.OrderBy(musica => musica.Artista).Select(musica => musica.Artista).Distinct().ToList();
+        Console.WriteLine("Lista de artista ordenados");
+        foreach (var artista in artistasOrdenados)
+        {
+            Console.WriteLine($"- {artista}");
+        }
+    }
 }
