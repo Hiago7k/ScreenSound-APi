027bc64c0c3c89e8024e204209bbbcf21da78402
Hiago 7k <hiago7k@gmail.com>
2026-03-18 18:33:32 -0300

    criando novo filtro

---
+++ Filtros/LinqOrder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSound_APi.Filtros;
+
+internal class LinqOrder
+{
+    public static void ExibirListaDeArtistasOrdernados() { }
+}
