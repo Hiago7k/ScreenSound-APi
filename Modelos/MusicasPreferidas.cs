8640c35d32791c21d36424b119f50e9edf026151
Hiago 7k <hiago7k@gmail.com>
2026-03-23 19:53:00 -0300

    iniciando a lista, com construtores com uma lista vazia, e um nome

--- Modelos/MusicasPreferidas.cs
+++ Modelos/MusicasPreferidas.cs
@@ -1,7 +1,13 @@
-﻿
-
-namespace ScreenSound_APi.Modelos;
+﻿namespace ScreenSound_APi.Modelos;
 
 internal class MusicasPreferidas
 {
+    public string? Nome { get; set; }
+    public List<Musica> ListaDeMusicasFavoritas { get; }
+
+    public MusicasPreferidas(string nome) 
+    {
+        Nome = nome;
+        ListaDeMusicasFavoritas = new List<Musica>();
+    }
 }
